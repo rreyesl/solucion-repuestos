eb4d2cac1c449059e90c87a347ae1e0204300934
rreyes <rreyes@WS_RREYES>
2018-12-07 01:18:52 -0300

    Cambio de estilos en vista

--- WebRepuestos/Clientee/Cliente1.Master.designer.cs
+++ WebRepuestos/Clientee/Cliente1.Master.designer.cs
@@ -31,13 +31,13 @@ namespace WebRepuestos.Cliente {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// Control Menu1.
+        /// Control Menu2.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu Menu1;
+        protected global::System.Web.UI.WebControls.Menu Menu2;
         
         /// <summary>
         /// Control ContentPlaceHolder1.
--- WebRepuestos/GerenteGeneral/Inicio.aspx
+++ WebRepuestos/GerenteGeneral/Inicio.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/GerenteGeneral/GerenteGeneral1.Master" AutoEventWireup="true" CodeBehind="Inicio2.aspx.cs" Inherits="WebRepuestos.GerenteGeneral.InicioGerenteGeneral" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/GerenteGeneral/GerenteGeneral1.Master" AutoEventWireup="true" CodeBehind="Inicio.aspx.cs" Inherits="WebRepuestos.GerenteGeneral.InicioGerenteGeneral" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
